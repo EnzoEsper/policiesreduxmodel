68147efa7823b40f12923adb07c286eb8f78df39
enzoesper <enzoesper@hotmail.com>
2019-09-05 12:44:27 -0300

    testing the example, combine all the reducers together and with the store object

--- model.js
+++ model.js
@@ -56,3 +56,47 @@ const accounting = (bagOfMoney = 100, action) => {
   // we dont care about this action
   return bagOfMoney;
 };
+
+const policies = (listOfPolicies = [], action) => {
+  if (action.type === "CREATE_POLICY") {
+    return [...listOfPolicies, action.payload.name];
+  } else if (action.type === "DELETE_POLICY") {
+    return listOfPolicies.filter(name => name !== action.payload.name);
+  }
+
+  return listOfPolicies;
+};
+
+const { createStore, combineReducers } = Redux;
+
+const ourDepartments = combineReducers({
+  accounting: accounting,
+  claimsHistory: claimsHistory,
+  policies: policies
+});
+
+const store = createStore(ourDepartments);
+
+// Example
+const action = createPolicy("Alex", 20);
+store.dispatch(action);
+
+// to get our repository of data and verify that the dispatch function worked
+console.log(store.getState());
+
+// So, now 1) we can call all of our different action creators
+// 2) we can pass the action thats are returned to the store.dispatch() function
+// 3) and that is going to modify the store of the application
+
+// another examples
+store.dispatch(createPolicy("Anna", 50));
+store.dispatch(createPolicy("Bob", 45));
+store.dispatch(createPolicy("Milton", 50));
+
+store.dispatch(createClaim("Bob", 120));
+store.dispatch(createPolicy("Anna", 30));
+
+store.dispatch(deletePolicy("Alex"));
+
+// to get our repository of data and verify that the dispatch function worked
+console.log(store.getState());
